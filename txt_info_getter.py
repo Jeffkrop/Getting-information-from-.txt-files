4f9d3cdeb649574e9f6a29b4a0ad3bca65ede404
Jeffkrop <jeffkrop@gmail.com>
2016-04-10 13:51:31 -0500

    updates

--- txt_info_getter.py
+++ txt_info_getter.py
@@ -1,3 +1,4 @@
+
 #
 # Author Jeff Kropelnicki
 # Date created February 28, 2016
@@ -5,7 +6,10 @@
 #
 # This is code a reading text files to look for the occurrence of words. 
 # For this project I will be using the words to build a database of locations 
-# to be used for making a map using ArcGIS. 
+# to be used for making a map using ArcGIS. The code will also return a list of 
+# all words used in the document and the number of times they occure.
+
+
 # I added a fuction that will take a loction and print the address and latitude and longitude.
 
 
@@ -18,27 +22,37 @@ def read_file(file):
     '''
         This is a function that asks the user to input a file path, 
         asks the user to input the word they are looking for in the file, 
-        print the lines the work occurs in. 
-        It then counts the number of times the word is used in the file.
+        print the lines where the work occurs. 
+        It then give an output of all of the words in the user inputted text file 
+        with the number of times the word occurs.
+        It then counts the number of times the user chosen word is used in the file.
+        
     '''
-    #reads in the file and print the lines that have to word. 
+    #reads in the file, asks users for a word they 
+    #are searching for then print the lines that have to word.
+    #followed by all of the words in the file and how many times 
+    #each word is used.
     file_name = raw_input("Enter the .txt file path: ")
     with open(file_name) as f:
         the_word = raw_input("Enter the word you are searching for: ")
         print ""
-        word_list = []
         for line in f:
             if the_word in line:
                 print line 
-        
+                
+                
+    #Assigns each word in the txt document to a list, converts to
+    #lower case, splits and appends each word.    
     with open(file_name) as f:
+        word_list = []
         for words in f:
             content = str(words)
             word_f = content.lower().split()
             for each_word in word_f:
                 word_list.append(each_word)
+     
                       
-    """ This count the number of times the word that the user input occurs """
+    #Count and prints the number of times user given word occurs.
     total = 0
     with open(file_name) as f:
         for line in f:
@@ -47,16 +61,21 @@ def read_file(file):
                 total += 1
     print "The word", str(the_word), "occurs", total, "times\n"
     
+    
+    #Names for the below functions geopy and clean_list to call.
     geopy(the_word)
-    clean_list(word_list)
+    clean_list(word_list)    
+
     
 
     
     
 def geopy(the_word): 
-    """ This function take in a user given location and gives back the address with 
+    
+    """ This function returns the address with 
     city, zip code, state, county and country.
-    It also provides latitude and longitude. """
+    It also provides latitude and longitude of 
+    the user inputted word from above."""
     
     geolocator = Nominatim()
     location = geolocator.geocode(the_word)
@@ -68,18 +87,30 @@ def geopy(the_word):
  
 
 def clean_list(word_list):
+    
+    """This function take the list of words in 
+    the above list and replaces
+    and thing that is not a letter or number 
+    with a empty. It then creates a new list"""
+    
     clean_word_list = []
     for word in word_list:
         symbols = "!@#$%^&*()_+={}|\/?><,.-- \" \' \" \' "
         for i in range(0, len(symbols)):
             word = word.replace(symbols[i], "")
         if len(word) > 0:
-            #print word
             clean_word_list.append(word)
+    
+    
     create_dict(clean_word_list)
             
 
 def create_dict(clean_word_list):
+    
+    """Creates a new dictionary of the cleaned 
+    list of words from the text document
+    and counts the number of times the word occurs."""
+    
     word_count = {}
     for word in clean_word_list:
         if word in word_count:
@@ -107,3 +138,4 @@ read_file(file)
 
                   
                     
+
